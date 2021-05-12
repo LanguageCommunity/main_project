a8f34d3a469f42d76a4f7fb2a72cd7f29b7c04c5
Raphael Marques <raphaeljcmarques@gmail.com>
2021-05-11 23:07:49 -0300

    PI Project - Raphael

---
+++ Language Community/Código PI/PI_Project/src/user/Premium.java
@@ -0,0 +1,19 @@
+package user;
+
+public class Premium {
+
+private String desPacotePremium;
+	
+	public Premium(String desPacotePremium) {
+		setDesPacotePremium(desPacotePremium);
+	}
+
+	public String getDesPacotePremium() {
+		return desPacotePremium;
+	}
+
+	public void setDesPacotePremium(String desPacotePremium) {
+		this.desPacotePremium = desPacotePremium;
+	}
+	
+}
